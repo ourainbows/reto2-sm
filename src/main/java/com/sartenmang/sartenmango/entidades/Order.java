349ec08e0301b96a869ec4f90df508ecf3c9160b
ourainbows <casasjuanfelipe@gmail.com>
2021-12-06 15:01:51 -0500

    Creada la entidad de ordenes

---
+++ src/main/java/com/sartenmang/sartenmango/entidades/Order.java
@@ -0,0 +1,28 @@
+package com.sartenmang.sartenmango.entidades;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    public static String PENDING = "Pendiente";
+    public static String APROVED = "Aprobada";
+    public static String REJECTED = "Rechazada";
+    @Id
+    private Integer id;
+    private Date registerDay;
+    private String status;
+    private User salesMan;
+    private Map<String, Cookware> products;
+    private Map<String, Integer> quantities;
+}
--- src/main/java/com/sartenmang/sartenmango/entidades/User.java
+++ src/main/java/com/sartenmang/sartenmango/entidades/User.java
@@ -1,6 +1,5 @@
 package com.sartenmang.sartenmango.entidades;
 
-import java.util.Date;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
