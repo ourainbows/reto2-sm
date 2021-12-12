8bb680e1f1b1dbbca5ec33506f7ba627fd710783
ourainbows <casasjuanfelipe@gmail.com>
2021-12-12 00:24:50 -0500

    Vista basica del coordinador, con posibilidad de actualizar

---
+++ src/main/resources/static/js/coord.js
@@ -0,0 +1,49 @@
+function getOrdenes(zona) {
+    $.ajax({
+        //url: "http://150.230.86.64:81/api/order/zona"+ zona,
+        url: "http://localhost:8080/api/order/zona/" + zona,
+        type: "GET",
+        dataType: "json",
+        success: function (answer) {
+            console.log(answer)
+            drawOrders(answer);
+        },
+    });
+}
+function drawOrders(orders) {
+    for (i = 0; i < orders.length; i++) {
+        let pedidos = "<div class='card pedido' style='width: 18rem;'>"
+        pedidos += "<div class='card-body'>"
+        pedidos += "<h5 class='card-title'>" + orders[i].salesMan.name+"</h5>"
+        pedidos += "<h5 class='card-subtitle mb-2 text-muted'>" + orders[i].registerDay + "</h5>"
+        pedidos += "<p class='card-text'>" + JSON.stringify(orders[i].quantities)  + "</p>"
+        pedidos += "<button class='btn btn-primary' onclick='updateOrder(" + orders[i].id + ")'>Aprobar</button>"
+        /*  pedidos += "<select class='form-control' id='select-" + orders.id + "'>"
+            pedidos += "<option selected='selected'>Pendiente</option>"
+            pedidos += "<option selected='selected'>Aprobada</option>"
+            pedidos += "<option selected='selected'>PENDIENTE</option>"
+            pedidos += "</select>" */
+        pedidos += "</div>"
+        pedidos += "</div>"
+        $("#ordenes").html(pedidos);
+    }
+}
+function updateOrder(idOrden) {
+    let orden = {
+        id: idOrden,
+        status: "Aprobada"
+    }
+    $.ajax({
+        //url: "http://150.230.86.64:81/api/user/update",
+        url: "http://localhost:8080/api/order/update",
+        type: "PUT",
+        data: JSON.stringify(orden),
+        contentType: "application/JSON",
+        dataType: "json",
+        complete: function () {
+            console.log("Orden Actualizada");
+        },
+    });
+}
+
+getOrdenes(localStorage.getItem("zona"))
---
+++ src/main/resources/static/login.js
@@ -0,0 +1,59 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          event.preventDefault();
+          logIn();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+function logIn() {
+  $.ajax({
+    url:
+      "http://localhost:8080/api/user/" +
+      //"http://150.230.86.64:81/api/user/" +
+      $("#email-login").val() +
+      "/" +
+      $("#password-login").val(),
+    type: "GET",
+    dataType: "json",
+    success: function (answer) {
+      if (answer.name != "NO DEFINIDO") {
+        $("#estadoUsuario").text("Sesión Iniciada correctamente");
+    
+        localStorage.setItem("correo", answer.email)
+        localStorage.setItem("contraseña", answer.password)
+        localStorage.setItem("zona", answer.zone)
+
+        if (answer.type == "ADM") {
+          $(location).attr("href", "admin.html");
+        }
+        if (answer.type == "COORD") {
+          $(location).attr("href", "coord.html");
+        }
+        if (answer.type == "ASE") {
+          $(location).attr("href", "ase.html");
+        }
+      } else {
+        $("#estadoUsuario").text("Su correo o contraseña es incorrecto");
+      }
+    },
+  });
+}
